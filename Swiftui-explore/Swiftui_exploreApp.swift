0146668355893f57f5a0cd6c76929b4df244d5e9
Muzahidul Islam <mmsaddam@gmail.com>
2022-12-17 16:25:11 +0600

    Initial Commit

---
+++ Swiftui-explore/Swiftui_exploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftui_exploreApp.swift
+//  Swiftui-explore
+//
+//  Created by Muzahid on 17/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Swiftui_exploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
